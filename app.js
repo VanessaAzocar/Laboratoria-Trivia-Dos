c68ef3cb1b754cdf534afbe1b5f6662b05fce3c4
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-05-19 12:14:28 -0300

    Los botones muestran respuesta en un parrafo y preguntas en negrita

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 // Respuesta SI pregunta 1
     function myFunction() {
-      document.getElementById("uno-s").innerHTML = "¡Incorrecto! No han Ingresado Hombres de Laboratoria ";
+      document.getElementById("respuesta-uno-s").innerHTML = "¡Incorrecto! No han Ingresado Hombres de Laboratoria ";
     }
     // Respuesta NO regunta 1
        function myFunction2() {
-      document.getElementById("uno-n").innerHTML = "Correcto";
+      document.getElementById("respuesta-uno-n").innerHTML = "¡Correcto!";
     }
-    
+    // Respuesta SI pregunta 2
+    function myFunction3() {
+        document.getElementById("respuesta-dos-s").innerHTML = "¡Correcto!";
+      }
+      // Respuesta NO regunta 2
+         function myFunction4() {
+        document.getElementById("respuesta-dos-n").innerHTML = "¡Incorrecto! Laboratoria cuenta con cinco sedes en Latinoamerica";
+      }
+        // Respuesta SI pregunta 3
+    function myFunction5() {
+        document.getElementById("respuesta-tres-s").innerHTML = "¡Correcto!";
+      }
+      // Respuesta NO regunta 3
+         function myFunction6() {
+        document.getElementById("respuesta-tres-n").innerHTML = "¡Incorrecto! En Chile ya inicio la sexta generación";
+      }
